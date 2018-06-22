7ead6bce2c54314687429c35b6a15a6b736b53d0
Richmond-Truong <Richmond.t@hotmail.ca>
2018-06-22 18:06:31 -0400

    first commit

---
+++ client/src/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+        <div className = "test">
+            <h1> test 2</h1>
+        </div>
+    );
+  }
+}
+
+export default Home;
