15a83dc2d78c6d2c01342d0132f5cb2030e7754f
singha95 <angad.singh@mail.utoronto.ca>
2018-06-24 15:36:43 -0400

    Removed the home folder and merged with the client folder.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Home from "./Home/Home.js"
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
