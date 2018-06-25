80efaf06c31a3575854a877d2fdfe795984f0180
Angad Singh <angadsingh@evertz.com>
2018-06-25 09:48:00 -0400

    Added Code for topBar, But is not implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 class SideBar extends Component{
   render() { 
-    return ( 
+    return (
     <div id="mySidenav" class="sidenav">
-      <img class="logo" src={require('./images/logo.png')} /> 
+      <img class="logo" alt="" src={require('./images/logo.png')}/> 
       <a href="#About" id="about">About</a>
       <a href="#Projects" id="projects">Projects</a>
       <a href="#Contact" id="contact">Contact</a>
       
     </div>
-    )
+    );
   }
 }
-class test extends Component{ 
 
+class TopBar extends Component{ 
   render() { 
-      return (
-          <div className="App"> 
-              <h1>Hi, Im a React App </h1> 
-          </div>
-      );  
-      
+    return ( 
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+    );
   }
 }
+const HomePage = [
+  SideBar,
+  TopBar
+]
 
 export default SideBar; 
