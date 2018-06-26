7045a3eee19d893cef363073de237f8095893202
Angad Singh <angadsingh@evertz.com>
2018-06-26 09:22:02 -0400

    Made a functionally hidden side bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,60 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 
 
+
 class SideBar extends Component{
+
+  constructor(prop) {
+    super(prop);
+    this.open_side_bar = this.open_side_bar.bind(this)
+    this.close_side_bar = this.close_side_bar.bind(this)
+  }
+
+  open_side_bar() {
+    this.setState({width: '200px'})
+  }
+
+  close_side_bar() {
+    this.setState({width: '0px'})
+    
+  }
+
   render() { 
-    return (
-    <div id="mySidenav" class="sidenav">
-      <img class="logo" alt="" src={require('./images/logo.png')}/> 
+    return [ 
+    
+    <div id="mySidenav" className="sidenav" style={this.state}  key="1">
+      <a href="javascript:void(0)" className="closebtn" onClick={this.close_side_bar}>&times;</a>
+      <img className="logo" alt="" src={require('./images/logo.png')}/> 
       <a href="#About" id="about">About</a>
       <a href="#Projects" id="projects">Projects</a>
-      <a href="#Contact" id="contact">Contact</a>
-      
-    </div>
-    );
+      <a href="#Contact" id="contact">Contact</a> 
+    </div>,
+    <TopBar/>,
+    <div className="sideNavButton" href="javascript:void(0)" key="2" onClick={this.open_side_bar.bind(this)}>&#9776; open</div>,
+    ];
   }
 }
 
 class TopBar extends Component{ 
   render() { 
     return ( 
-    <div class="topnav">
-      <a class="active" href="#home">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
+    <div className="topnav">
+      <a href="#Settings">Settings</a>
+      <a className="active">Username</a>
     </div>
     );
   }
 }
-const HomePage = [
-  SideBar,
-  TopBar
-]
 
-export default SideBar; 
+class HomePage extends Component{
+  render() { 
+    return [
+      <SideBar key="1"/>, 
+    ]
+  }
+}
+
+
+
+export default HomePage; 
