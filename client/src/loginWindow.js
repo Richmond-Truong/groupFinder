012a50c13c8db8f4cbdaa425fbb6318371cf3252
Daniel Diep <daniel.diep49@gmail.com>
2018-06-27 00:03:59 -0400

    Added Login Modal

---
+++ client/src/loginWindow.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react';
+
+class loginWindow extends React{
+    render() {
+        return(
+           <div className= "login">
+                <button>Hello</button>
+           </div> 
+        );
+
+    }
+}
+export default loginWindow;
