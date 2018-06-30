7e16529f01007765ec4fd5470675e41190b0aa15
Daniel-Diep <daniel.diep49@gmail.com>
2018-06-30 17:16:34 -0400

    Added Login Page

--- client/src/loginWindow.js
+++ client/src/loginWindow.js
@@ -1,6 +1,76 @@
 import React, { Component }  from 'react';
-import Modal from './modal';
+import Modal from 'react-modal';
 
+class Loginwindow extends Component{
+    constructor(){
+      super();
+      this.state = { 
+        modalOn : false,
+        username: '',
+        password: '' 
+      };
+      this.openModal = this.openModal.bind(this);
+      this.closeModal = this.closeModal.bind(this);
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    openModal() {
+      this.setState({modalOn : true});
+    }
+
+    closeModal() {
+      this.setState({modalOn : false});
+    }
+
+    handleInputChange(event){
+        const target = event.target;
+        const name = event.target.name;
+        this.setState({[name]: target.value})
+    }
+
+    handleSubmit(event) {
+        alert('Username: ' + this.state.username + ' Password: ' + this.state.password);
+        event.preventDefault();
+    }
+
+
+    render() 
+    {
+      return(
+        <div>
+            <button className="open-modal-btn" onClick = {this.openModal}> Here </button>
+            <Modal  className="login-modal"
+                    overlayClassName="Overlay"
+                    isOpen={this.state.modalOn}
+                    onRequestClose={this.closeModal}
+            >
+                    <button className="close-modal-btn" onClick = {this.closeModal}> &#9747; </button>
+                    <h1> Sign in </h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <input class='usernameField' placeholder="Username" type="text" onChange={this.handleInputChange}/> 
+                        </label>
+                        <br/>
+                        <br/>
+                        <label>
+                            <input class='passwordField' placeholder="Password" type="password" onChange={this.handleInputChange}/>
+                        </label>
+                        <br/>
+                        <input class='attemptLogin' type="submit" value="Login"/>
+                    </form>
+                    <a href >New user? Click here to register</a>
+
+            </Modal>
+        </div>
+      );
+    }
+}
+
+export default Loginwindow;
+
+/*
 class LoginWindow extends Component{
     constructor(props) {
         super(props)
@@ -52,7 +122,7 @@ class LoginWindow extends Component{
         this.setState({ isModalOpen: false })
       }
     }
-/*
+
     render() {
         return(
            <div className="login">
@@ -72,4 +142,3 @@ class LoginWindow extends Component{
     }
 }
 */
-export default LoginWindow;
--- client/src/topBar.js
+++ client/src/topBar.js
@@ -1,15 +1,71 @@
 import React, { Component }  from 'react';
-
+import Modal from 'react-modal';
+import Loginwindow from './loginWindow.js';
 class TopBar extends Component{ 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      modalOn : false,
+      username: '',
+      password: '' 
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  openModal() {
+    this.setState({modalOn : true});
+  }
+
+  closeModal() {
+    this.setState({modalOn : false});
+  }
+
+  handleInputChange(event){
+      const target = event.target;
+      const name = event.target.name;
+      this.setState({[name]: target.value})
+  }
+
+  handleSubmit(event) {
+      alert('Username: ' + this.state.username + ' Password: ' + this.state.password);
+      event.preventDefault();
+  }
+
       render() { 
         return ( 
         <div className="topnav">
           <a href="#Settings">Settings</a>
           <a className="active">Username</a>
-          <a href="active" onClick = {() => alert('click')}>Login</a>
+          <a onClick = {this.openModal}>Login</a>
+          <Modal  className="login-modal"
+                    overlayClassName="Overlay"
+                    isOpen={this.state.modalOn}
+                    onRequestClose={this.closeModal}
+            >
+                    <button className="close-modal-btn" onClick = {this.closeModal}> &#9747; </button>
+                    <h1> Sign in </h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            <input class='usernameField' placeholder="Username" type="text" onChange={this.handleInputChange}/> 
+                        </label>
+                        <br/>
+                        <br/>
+                        <label>
+                            <input class='passwordField' placeholder="Password" type="password" onChange={this.handleInputChange}/>
+                        </label>
+                        <br/>
+                        <input class='rememberMe' type="checkbox"/> <h2>Remember me </h2>
+                        <br/>
+                        <input class='attemptLogin' type="submit" value="Login"/>
+                    </form>
+                    <a href >New user? Click here to register</a>
+            </Modal>
         </div>
         );
       }
 }
-
 export default TopBar;
