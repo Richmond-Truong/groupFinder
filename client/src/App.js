0711824504b7cca897335b20dcb3f6ff66b93700
Daniel-Diep <daniel.diep49@gmail.com>
2018-06-30 17:17:27 -0400

    Cleaned up some test code

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import './barStyle.css'
 import './loginWindow.css'
 import SideBar from './sideBars.js';
-import LoginWindow from './loginWindow';
 
 
 class HomePage extends Component{
   render() { 
     return [
       <SideBar key="1"/>, 
-      <LoginWindow/>,
     ]
   }
 }
