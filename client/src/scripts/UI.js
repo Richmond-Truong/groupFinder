cc60cec7607ed031d78ef05525d94211816f2550
singha95 <angad88@hotmail.com>
2018-07-19 09:52:36 -0400

    Removed all instances of the sidebar and removed any lingering logs

--- client/src/scripts/UI.js
+++ client/src/scripts/UI.js
@@ -2,48 +2,6 @@ import React, { Component } from 'react';
 import Modal from 'react-modal';
 import { Link } from 'react-router-dom';
 
-
-class SideBar extends Component {
-
-  constructor(prop) {
-    super(prop);
-    this.open_side_bar = this.open_side_bar.bind(this)
-    this.close_side_bar = this.close_side_bar.bind(this)
-  }
-
-  open_side_bar() {
-    this.setState({ width: '15%', minWidth: '200px' })
-  }
-
-  close_side_bar() {
-    this.setState({ width: '0%', minWidth: '0px' })
-
-  }
-
-
-
-  render() {
-
-    return [
-
-      <div id="mySidenav" className="sidenav" style={this.state} key="1">
-        <button className="closebtn" onClick={this.close_side_bar}>
-          &times;
-        </button>
-        <img className="logo" alt="" src={require('./../images/logo.png')} />
-        <a href="#About" id="about">About</a>
-        <a href="#Projects" id="projects">Projects</a>
-        <a href="#Contact" id="contact">Contact</a>
-      </div>,
-      <TopBar key='2' />,
-      <div key='3' style={{ width: '0px', height: '0px' }}>
-        <div className="sideNavButton" key='3' onClick={this.open_side_bar}>&#9776;</div>
-        <Link to='/' > <img alt='' className="homeButton" src={require('./../images/home.png')}></img> </Link>
-      </div>
-    ];
-  }
-}
-
 class TopBar extends Component {
   state = {
     modalOn: null,
@@ -70,7 +28,6 @@ class TopBar extends Component {
   }
 
   isModalOpen = (name) => {
-    console.log(`xxx modal = ${this.state.modalOn}`)
     return (name === this.state.modalOn);
   }
 
