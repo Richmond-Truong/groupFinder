60571a5fe77b1d911bf1fd021b29eb904c49b557
Daniel-Diep <daniel.diep49@gmail.com>
2018-07-19 22:43:44 -0400

    Updated View post page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import './styles/barStyle.css'
-import './styles/postStyle.css'
-import './styles/loginWindow.css'
-import UI from './scripts/UI';
-import ProjectPage from './scripts/viewProjects';
-import PostPage from './scripts/viewPost';
-=======
 import './styles/barStyle.css';
 import './styles/postStyle.css';
 import './styles/modalStyle.css';
 import './semantic/dist/semantic.css'
 import UI from './scripts/UI';
 import Main from './scripts/main';
->>>>>>> 0e9b2064ff7c39eed6379db5d8c4d94ef7cfda35
 
 class MainPage extends Component{
   render() { 
--- client/src/scripts/UI.js
+++ client/src/scripts/UI.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React, { Component }  from 'react';
-import Modal from 'react-modal';
-class SideBar extends Component{
-=======
 import React, { Component } from 'react';
 import Modal from 'react-modal';
 import { Link } from 'react-router-dom';
 
->>>>>>> 0e9b2064ff7c39eed6379db5d8c4d94ef7cfda35
 
 class SideBar extends Component {
 
@@ -50,88 +44,6 @@ class SideBar extends Component {
   }
 }
 
-<<<<<<< HEAD
-class TopBar extends Component{ 
-  constructor(props) {
-    super(props);
-    this.state = { 
-      modalOn : false,
-      username: '',
-      password: '' 
-    };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-  }
-  
-
-  /*
-    Function used to open the modal window
-  */
-  openModal() {
-    this.setState({modalOn : true});
-  }
-
-  /*
-    Function used to close the modal window
-  */
-  closeModal() {
-    this.setState({modalOn : false});
-  }
-
-  /*
-    Function used update the username and password state whenever there 
-    is an entry to the text boxes
-  */
-  handleInputChange(event){
-      const target = event.target;
-      const name = event.target.name;
-      this.setState({[name]: target.value})
-  }
-
-  /*
-    Function used to test the submit button
-  */
-  handleSubmit(event) {
-      alert('Username: ' + this.state.username + ' Password: ' + this.state.password);
-      event.preventDefault();
-  }
-
-  render() { 
-        return ( 
-          <div className="topnav">
-          <a href="#Settings">Settings</a>
-          <a className="active">Username</a>
-          <a onClick = {this.openModal}>Login</a>
-          <Modal  className="login-modal"
-                    overlayClassName="Overlay"
-                    isOpen={this.state.modalOn}
-                    onRequestClose={this.closeModal}
-          >
-                    <button className="close-modal-btn" onClick = {this.closeModal}> &#9747; </button>
-                    <h1> Sign in </h1>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            <input class='usernameField' placeholder="Username" type="text" onChange={this.handleInputChange}/> 
-                        </label>
-                        <br/>
-                        <br/>
-                        <label>
-                            <input class='passwordField' placeholder="Password" type="password" onChange={this.handleInputChange}/>
-                        </label>
-                        <br/>
-                        <input class='rememberMe' type="checkbox"/> <h2>Remember me </h2>
-                        <br/>
-                        <input class='attemptLogin' type="submit" value="Login"/>
-                    </form>
-                    <a href >New user? Click here to register</a>
-        </Modal>
-        </div>
-        );
-      }
-=======
 class TopBar extends Component {
   state = {
     modalOn: null,
@@ -261,7 +173,6 @@ class TopBar extends Component {
       // </div>
     );
   }
->>>>>>> 0e9b2064ff7c39eed6379db5d8c4d94ef7cfda35
 }
 
 
