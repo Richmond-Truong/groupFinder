6a21b4e69bf8a187374478c01913e18ef75d4d7f
Jacky Truong <jacky.truong227@gmail.com>
2018-07-29 17:05:38 -0400

    sign up is now inside one form

--- client/src/scripts/SignUp.js
+++ client/src/scripts/SignUp.js
@@ -8,46 +8,38 @@ class SignUp extends Component {
     render() {
         return (
         <div className="page">
-            <Header block id="box">
-                <Button id = "clicky">
-                <i class="facebook icon"></i>
-                    Login With Facebook
-                </Button> 
-                <Button id = "clicky2">
-                    Sign Up
-                </Button>
-                <Form id='form7'>
-                <input type = 'text' placeholder='Email' className="boxes"/>
-                    </Form>
-                <Form id ="form6">
-                <input type='text' placeholder='Username' className="boxes"/>
-                    </Form>
-                <Form id="form3">
-                <input type= "text" placeholder = 'Password' className = "boxes"/>
-                    </Form>
-                <Form id = "form4">
-                <input type = "text" placeholder = 'Confirm Password' className = "boxes"/> 
+                <Form id="box">
+                    <Button id = "clicky">
+                    <i class="facebook icon"></i>
+                        Login With Facebook
+                    </Button> 
+                    <Button id = "clicky2">
+                        Sign Up
+                    </Button>
+                    <input type = 'text' placeholder='Email' className="boxes" id='form7'/>
+                    <input type='text' placeholder='Username' className="boxes" id ="form6"/>
+                    <input type= "text" placeholder = 'Password' className = "boxes" id = "form3"/>
+                    <input type = "text" placeholder = 'Confirm Password' className = "boxes" id= "form4"/> 
+                    <input type = "text" placeholder = "Skills" className= "boxes" id="form5"/>
+                    <Header id="name">
+                        Group Finder
+                    </Header>
+                    <Header id="or">
+                        ------------------- Or -------------------
+                        </Header>
+
+
+                    <Header block id="smallbox">
+                        <Header id="terms">
+                            By signing up, you agree to our 
+                            <a href=''> terms of service.</a>
+                        </Header>
+                        </Header>
+                    <Header id="account">
+                        Already have an account? 
+                        <Link to='/login'> Sign in </Link>
+                    </Header>
                 </Form>
-                <Form id="form5">
-                <input type = "text" placeholder = "Skills" className= "boxes"/>
-                    </Form>
-                <Header id="name">
-                    Group Finder
-                </Header>
-                <Header id="or">
-                    ------------------- Or -------------------
-                    </Header>
-                <Header id="terms">
-                    By signing up, you agree to our 
-                    <a href=''> terms of service.</a>
-                    </Header>
-                </Header>
-                <Header block id="smallbox">
-                <Header id="account">
-                    Already have an account? 
-                    <Link to='/login'> Sign in </Link>
-                </Header>
-            </Header>
         </div>
 
 
