faa04b4e6a5d1d1ac7f6cf80c3c6ee44cf73a972
Jacky Truong <jacky.truong227@gmail.com>
2018-07-29 22:30:10 -0400

    added in appropriate validations to the sign up page

--- client/src/scripts/SignUp.js
+++ client/src/scripts/SignUp.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import {Button ,  Header , Form} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+function checkPasswords(password,confirmation){
+    if(document.getElementById("form3").value != document.getElementById("form4").value){
+        document.getElementById("form4").style.backgroundColor="pink"
+        document.getElementById("form3").style.backgroundColor="pink"
+        document.getElementById("clicky2").disabled = true;
+    }
+    else{
+        document.getElementById("form4").style.backgroundColor="white"
+        document.getElementById("form3").style.backgroundColor="white"
+        document.getElementById("clicky2").disabled = false;
+    }
+}
+
 
 class SignUp extends Component {
 
@@ -10,31 +23,28 @@ class SignUp extends Component {
         <div className="page">
                 <Form id="box">
                     <Button id = "clicky">
-                    <i class="facebook icon"></i>
+                        <i class="facebook icon"></i>
                         Login With Facebook
                     </Button> 
                     <Button id = "clicky2">
                         Sign Up
                     </Button>
-                    <input type = 'text' placeholder='Email' className="boxes" id='form7'/>
-                    <input type='text' placeholder='Username' className="boxes" id ="form6"/>
-                    <input type= "text" placeholder = 'Password' className = "boxes" id = "form3"/>
-                    <input type = "text" placeholder = 'Confirm Password' className = "boxes" id= "form4"/> 
-                    <input type = "text" placeholder = "Skills" className= "boxes" id="form5"/>
+                    <input type='text' placeholder='Username' className="boxes" id ="form6" maxLength="16" minLength="2"/>
+                    <input type = 'text' placeholder='Email' className="boxes" id='form7' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}" title="Not a Valid Email"/>
+                    <input type= "password" placeholder = 'Password' className = "boxes" id = "form3" onChange={checkPasswords}/>
+                    <input type = "password" placeholder = 'Confirm Password' className = "boxes" id= "form4" onChange={checkPasswords} /> 
                     <Header id="name">
                         Group Finder
                     </Header>
                     <Header id="or">
                         ------------------- Or -------------------
-                        </Header>
-
-
+                    </Header>
                     <Header block id="smallbox">
                         <Header id="terms">
                             By signing up, you agree to our 
                             <a href=''> terms of service.</a>
                         </Header>
-                        </Header>
+                    </Header>
                     <Header id="account">
                         Already have an account? 
                         <Link to='/login'> Sign in </Link>
