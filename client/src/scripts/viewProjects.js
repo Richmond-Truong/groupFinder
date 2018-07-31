90c20cbe4b313dcb3162b9d17f662e7cc53216ca
singha95 <angad88@hotmail.com>
2018-07-31 10:51:55 -0400

    Removed ad bar and fixed so that posts are centered

--- client/src/scripts/viewProjects.js
+++ client/src/scripts/viewProjects.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 class ProjectsPage extends Component {
     /**
-     * A container for all the posting for a project. Contains functions 
-     * for adding a post and to retrieve data from the datbase. 
+     * A container for all the posting for a project. Contains functions
+     * for adding a post and to retrieve data from the datbase.
      */
     constructor(prop) {
         super(prop);
@@ -18,7 +18,7 @@ class ProjectsPage extends Component {
     componentDidMount(){
         this.loadData();
     }
-    
+
     state = {
         data: [],
         loading:true
@@ -33,15 +33,15 @@ class ProjectsPage extends Component {
 
     createPost(user, title, text) {
         /**
-         * Will create a post for rendering given the required fields. 
+         * Will create a post for rendering given the required fields.
          */
         this.KEY = this.KEY + 1;
         var newPost = new Post(user, title, text);
         return (
-            <Link key={this.KEY.toString()} 
-                onClick={() => this.storePost("post", newPost.getJSON())} 
+            <Link key={this.KEY.toString()}
+                onClick={() => this.storePost("post", newPost.getJSON())}
                 to={{ pathname: `/post/${user}` }} style={{ textDecoration: 'none', color: 'black' }}>
-                
+
                 {newPost.render()}
             </Link>
         );
@@ -50,7 +50,7 @@ class ProjectsPage extends Component {
     loadData() {
         /**
          * Function used to fetch data from the server. Sets data variable to hold all necessary information
-         * about all the posts stored. 
+         * about all the posts stored.
          */
         fetch('https://groupfinder1.herokuapp.com/post')
             .then(response => response.json())
@@ -59,15 +59,15 @@ class ProjectsPage extends Component {
                 for (i = 0; i < data.length; i++) {
                     this.posts_list.push(this.createPost(data[i]['tags'], data[i]['title'], data[i]['description']));
                 }
-                setTimeout( () => this.setState({ data: this.posts_list, loading:false}), 500); 
+                setTimeout( () => this.setState({ data: this.posts_list, loading:false}), 500);
             })
             .catch(err => this.setState({ data: this.createPost("Sample User", this.props.url, err.toString()) }))
     }
 
     render() {
         const { loading } = this.state;
-        
-    
+
+
         if (loading) {
             return (
                 <div className="page">
@@ -79,23 +79,17 @@ class ProjectsPage extends Component {
                             </div>
                             <p>We're fetching that content for you.</p>
                         </div>
-                    </div> 
+                    </div>
                 </div>
             )
         }else {
             return (
                 <div className="page">
-                    
-                    
-                
                     {this.posts_list}
-                    <div class="ui wide skyscraper test ad" data-text="Wide Skyscraper" style={{position:"absolute", 
-                        width:"15%", right:"10px", top:"0px"}}></div>
-                    
                 </div>
             )
         }
-        
+
     }
 }
 
