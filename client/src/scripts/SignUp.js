fe5df28f1f81cd30ca77ec53335c23acdaed0803
Richmond-Truong <Richmond.t@hotmail.ca>
2018-08-03 11:41:30 -0400

    hooked up signup

--- client/src/scripts/SignUp.js
+++ client/src/scripts/SignUp.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import {Button ,  Header , Form, Popup, Icon} from 'semantic-ui-react';
+import {Button ,  Header , Form, Popup} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+const querystring = require('querystring');
+var http = require('http');
 
 function checkPasswords(){
-    if(document.getElementById("form3").value != document.getElementById("form4").value){
+    if(document.getElementById("form3").value !== document.getElementById("form4").value){
         document.getElementById("form4").style.backgroundColor="pink"
         document.getElementById("form3").style.backgroundColor="pink"
         document.getElementById("clicky2").disabled = true;
@@ -14,10 +16,46 @@ function checkPasswords(){
         document.getElementById("clicky2").disabled = false;
     }
 }
-
+function signUp(){
+    var postData = JSON.stringify({
+        'username' : document.getElementById("form6").value,
+        'password' : document.getElementById("form4").value,
+        'email' : document.getElementById("form7").value
+    });
+    var post_options = {
+        host: 'groupfinder1.herokuapp.com',
+        port: '80',
+        path: '/addUser',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            if(chunk.includes("Inserted user into database")){
+                console.log('Response: ' + chunk);
+            }else{
+                alert("Error could not input user");
+            }
+        });
+        res.on('error', function (error) {
+            console.log(`error = ${error}`);
+        });
+    });
+    post_req.on('error', (e) =>{
+        console.log(e);
+    })
+    post_req.write(postData);
+    post_req.end();
+}
 
 class SignUp extends Component {
-
+    
+  
+    
     render() {
         return (
         <div className="page">
@@ -26,7 +64,7 @@ class SignUp extends Component {
                         <i class="facebook icon"></i>
                         Login With Facebook
                     </Button> 
-                    <Button id = "clicky2">
+                    <Button id = "clicky2" onClick={signUp} >
                         Sign Up
                     </Button>
                     <input type='text' placeholder='Username' className="boxes" id ="form6" maxLength="16" minLength="2"/>
